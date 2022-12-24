262be6d79a3b62ea77a75c027d4d4b4d10d465c8
Ganisher <ganisherkhamrayev@gmail.com>
2022-12-25 03:27:03 +0500

    feat: add section with slider

---
+++ script.js
@@ -0,0 +1,29 @@
+// const inputCard = document.querySelectorAll(".ml-form input");
+// const labelCard = document.querySelectorAll(".ml-form label");
+
+// key.oninput = function () {
+//   for (let key of Array.from(inp)) {
+//     if (key.checked) {
+//       if (Array.from(inputCard).indexOf(key) == 2) {
+//         Array.from(labelCard)[2].style.top = "2%";
+//       } else if (Array.from(inputCard).indexOf(key) == 1) {
+//         Array.from(labelCard)[1].style.top = "-50%";
+//       }
+//     }
+//   }
+// };
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+});
